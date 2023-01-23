1ed8c5b3a3cbd95c0a78829532b658deb781212b
Grayson-Wills <gmwills@utexas.edu>
2023-01-23 12:47:01 -0600

    first server test

---
+++ server/server-main.py
@@ -0,0 +1,23 @@
+import sys
+import socket
+import selectors
+import types
+
+sel = selectors.DefaultSelector()
+
+# ...
+
+HOST = "100.75.56.66"  # Standard loopback interface address (localhost)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
