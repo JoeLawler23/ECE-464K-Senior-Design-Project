7d1eacb1bb392cbbbd3ff2ebdf27f9ee2d2b2038
kcc2473 <kazimircp@gmail.com>
2023-01-23 13:18:21 -0600

    test for led

--- robot/GPIO-TEST.py
+++ robot/GPIO-TEST.py
@@ -1,1 +1,17 @@
+import RPi.GPIO as GPIO
+from time import sleep
+GPIO.setmode(GPIO.BOARD)
+supply = 1
+
+GPIO.setup(supply,GPIO.OUT)
+
+GPIO.output(supply,GPIO.HIGH)
+
+sleep(2)
+//turning off
+GPIO.output(supply,GPIO.LOW)
+
+//reset
+GPIO.cleanup()
+
 print("Hello")
