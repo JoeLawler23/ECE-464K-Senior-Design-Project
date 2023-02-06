66f1eeca43d68b8618c0af8c11994f150ef82cc1
server <josephlawler23@utexas.edu>
2023-02-06 14:21:49 -0600

    multithreading (mulit-agent connection) implemented

--- server/server-main.py
+++ server/server-main.py
@@ -1,9 +1,13 @@
-import sys, termios, tty, os, time
+import sys, termios, tty, os
 import socket
 import selectors
 import types
 import keyboard
 import json
+import threading
+import signal
+import time
+import multiprocessing
 sel = selectors.DefaultSelector()
 
 # ...
@@ -14,51 +18,15 @@ sel = selectors.DefaultSelector()
 HOST = "100.107.15.32"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
-#def accept_wrapper(sock):
-#    conn, addr = sock.accept()  # Should be ready to read
-#    print(f"Accepted connection from {addr}")
-#    conn.setblocking(False)
-#    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
-#    events = selectors.EVENT_READ | selectors.EVENT_WRITE
-#    sel.register(conn, events, data=data)
-#    conn.sendall(b"data")
+portsInUse = []
+threads = []
+mySocket = 0
 
-#def service_connection(key, mask):
-#    sock = key.fileobj
-#    data = key.data
-#    if mask & selectors.EVENT_READ:
-#        recv_data = sock.recv(1024)  # Should be ready to read
-#        if recv_data:
-#            data.outb += recv_data
-#        else:
-#            print(f"Closing connection to {data.addr}")
-#            sel.unregister(sock)
-#            sock.close()
-#    if mask & selectors.EVENT_WRITE:
-#        if data.outb:
-#            print(f"Echoing {data.outb!r} to {data.addr}")
-#            sent = sock.send(data.outb)  # Should be ready to write
-#            data.outb = data.outb[sent:]
+ 
+def handler(signum, frame):
+    mySocket.close()
+    sys.exit()
 
-#lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#lsock.bind((HOST, PORT))
-#lsock.listen()
-#print(f"Listening on {(HOST, PORT)}")
-#lsock.setblocking(False)
-#sel.register(lsock, selectors.EVENT_READ, data=None)
-
-#try:
-#    while True:
-#        events = sel.select(timeout=None)
-#        for key, mask in events:
-#            if key.data is None:
-#                accept_wrapper(key.fileobj)
-#            else:
-#                service_connection(key, mask)
-#except KeyboardInterrupt:
-#    print("Caught keyboard interrupt, exiting")
-#finally:
-#    sel.close()
 def getch():
     fd = sys.stdin.fileno()
     old_settings = termios.tcgetattr(fd)
@@ -70,12 +38,7 @@ def getch():
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
     return ch
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-   
-
+def myThread(conn, addr, s, port):
     with conn:
         print(f"Connected by {addr}")
         while True:
@@ -98,4 +61,22 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             else:
                 print(char)
 
-    s.close()
+
+
+signal.signal(signal.SIGINT, handler)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    mySocket = s
+    while True:
+        s.listen()
+        conn, addr = s.accept()
+        thisThread = threading.Thread(target = myThread, args = (conn, addr, s, PORT))
+        thisThread.daemon = True
+        thisThread.start()
+        threads.append(thisThread)
+for i in threads:
+    i.join()
+s.close()
+
+        
