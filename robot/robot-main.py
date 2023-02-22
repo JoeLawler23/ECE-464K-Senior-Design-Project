4819d31a30037af1f2a5f264e1daf3972f9e1f46
Grayson-Wills <gmwills@utexas.edu>
2023-02-22 12:15:21 -0600

    Fixed JSON protocol and added turn

--- robot/robot-main.py
+++ robot/robot-main.py
@@ -19,91 +19,85 @@ PORT = 65432  # The port used by the server
 left_motor = hardware_init.Motor(2, 3, 4, 17, 14, 15)
 right_motor = hardware_init.Motor(19, 26, 20, 21, 5, 6)
 robot = hardware_init.Robot(left_motor, right_motor)
-robot.drive(hardware_init.Direction.FORWARD, 100, 0.25)
-time.sleep(0.5)
-robot.drive(hardware_init.Direction.REVERSE, 100, 0.25)
-time.sleep(0.5)
-robot.turn(hardware_init.Direction.CLOCKWISE, 100, 90)
-time.sleep(0.5)
-robot.drive(hardware_init.Direction.FORWARD, 100, 0.25)
-time.sleep(0.5)
-robot.drive(hardware_init.Direction.REVERSE, 100, 0.25)
-time.sleep(0.5)
-robot.turn(hardware_init.Direction.COUNTER_CLOCKWISE, 100, 90)
-time.sleep(0.5)
+# robot.drive(hardware_init.Direction.FORWARD, 100, 0.25)
+# time.sleep(0.5)
+# robot.drive(hardware_init.Direction.REVERSE, 100, 0.25)
+# time.sleep(0.5)
+# robot.turn(hardware_init.Direction.CLOCKWISE, 100, 90)
+# time.sleep(0.5)
+# robot.drive(hardware_init.Direction.FORWARD, 100, 0.25)
+# time.sleep(0.5)
+# robot.drive(hardware_init.Direction.REVERSE, 100, 0.25)
+# time.sleep(0.5)
+# robot.turn(hardware_init.Direction.COUNTER_CLOCKWISE, 100, 90)
+# time.sleep(0.5)
 
-# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as pi:
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as pi:
 
-#     # Init connection
-#     connected = False
+    # Init connection
+    connected = False
 
-#     # Attempt to connect to server
-#     while not connected:
-#         try:
-#             pi.connect((HOST, PORT))
-#             connected = True
-#         except:
-#             connected = False
+    # Attempt to connect to server
+    while not connected:
+        try:
+            pi.connect((HOST, PORT))
+            connected = True
+        except:
+            connected = False
 
-#     # Connected to server
-#     while True:
+    # Connected to server
+    while True:
 
-#         # Receive data
-#         data = pi.recv(1024)
+        # Receive data
+        data = pi.recv(1024)
 
-#         # Load in data
-#         command = json.loads(data)
+        print(data)
 
-#         # Command structure
-#         # COMMAND_TYPE - DRIVE,TURN,STOP
-#         if (command["COMMAND_TYPE"] == "DRIVE"):
-#             # DRIVE
-#             # Direction - FORWARD,REVERSE
-#             # Distance(cm) - #
-#             # Speed - 0-100
-#             direction = hardware_init.Direction(command["DIRECTION"])
-#             distance = command["DISTANCE"]
-#             speed = command["SPEED"]
-#             print(f"Drive command received: Direction:{direction}, Distance:{distance}, Speed:{speed}",)
+        # Load in data
+        command = json.loads(data)
 
-#             # Drive
-#             robot.drive(direction, speed, distance)
+        # Command structure
+        # COMMAND_TYPE - DRIVE,TURN,STOP
+        if (command["COMMAND_TYPE"] == "DRIVE"):
+            # DRIVE
+            # Direction - FORWARD,REVERSE
+            # Distance(cm) - #
+            # Speed - 0-100
+            direction = hardware_init.Direction(command["DIRECTION"])
+            distance = command["DISTANCE"]
+            speed = command["SPEED"]
+            print(f"Drive command received: Direction:{direction}, Distance:{distance}, Speed:{speed}",)
+
+            # Drive
+            robot.drive(direction, speed, distance)
             
 
-#         elif(command["COMMAND_TYPE"] == "TURN"):
-#             # TURN
-#             # Heading - 0-360
-#             # Speed - 0-100
-#             # Direction - LEFT,RIGHT
-#             heading = command["HEADING"]
-#             speed = command["SPEED"]
-#             direction = hardware_init.Direction[command["DIRECTION"]]
+        elif(command["COMMAND_TYPE"] == "TURN"):
+            # TURN
+            # Heading - 0-360
+            # Speed - 0-100
+            # Direction - LEFT,RIGHT
+            degrees = command["DEGREES"]
+            speed = command["SPEED"]
+            direction = hardware_init.Direction(command["DIRECTION"])
 
-#             # Calculate distance based on heading
-#             # TODO
-#             distance = 0
+            # Calculate distance based on heading
+            robot.turn(direction, speed, degrees)
+            print(f"Turn command received: Direction:{direction}, Degrees:{degrees}, Speed:{speed}",)
 
-#             # Turn left or right
-#             if(direction == "LEFT"):
-#                 # TODO 
-#                 pass
-#             elif(direction == "RIGHT"):
-#                 # TODO
-#                 pass
+        elif(command["COMMAND_TYPE"] == "STOP"):
+            # STOP
+            # No args
+            robot.stop()
 
-#         elif(command["COMMAND_TYPE"] == "STOP"):
-#             # STOP
-#             # No args
-#             robot.stop()
-
-#         # Send back
-#         # Hits something
-#         # Stops
-#         # Kinect
+        # Send back
+        # Hits something
+        # Stops
+        # Kinect
 
 
-#print(f"Received {data!r}")
-#sel = selectors.DefaultSelector()
-#messages = [b"Message 1 from client.", b"Message 2 from client."]
-#start_connections(HOST, PORT, 2)
-#print("made it here")
+# print(f"Received {data!r}")
+# sel = selectors.DefaultSelector()
+# messages = [b"Message 1 from client.", b"Message 2 from client."]
+# start_connections(HOST, PORT, 2)
+# print("made it here")
