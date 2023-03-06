456c668d0e8988b9a63fc691444c12fe189e2fce
Grayson-Wills <gmwills@utexas.edu>
2023-03-06 13:47:29 -0600

    Kinect now shows video and depth data

---
+++ robot/Kinect.py
@@ -0,0 +1,34 @@
+import freenect
+import numpy as np
+import readchar
+import time
+import matplotlib.pyplot as plt
+
+# # DEMO
+# freenect-glview
+
+# Initialize the plot
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 4))
+
+# Update the subplots in real time
+while True:
+    # Capture a color frame and a depth frame from the Kinect sensor
+    video, _ = freenect.sync_get_video()
+    depth, _ = freenect.sync_get_depth()
+
+    # Convert the video data to an RGB image
+    rgb_image = video[:, :, ::-1]
+
+    # Display the color image as a subplot
+    ax1.clear()
+    ax1.imshow(rgb_image)
+    ax1.set_title('Color Image')
+
+    # Display the depth map as a subplot
+    ax2.clear()
+    ax2.imshow(depth, cmap='gray')
+    ax2.set_title('Depth Map')
+
+    # Update the subplots with the latest data
+    plt.draw()
+    plt.pause(0.001)
